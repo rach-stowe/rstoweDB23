2ea01965618b2a31d30e2602d834f2da1a672370
Rachel <rachelstowe2020@gmail.com>
2023-09-27 15:19:33 +0000

    Drafted tables in SQL script

--- lab05/lab05.sql
+++ lab05/lab05.sql
@@ -9,7 +9,25 @@ October 2, 2023
 CREATE DATABASE carpet_store;
    USE carpet_store;
 
-/* Create Carpets table to store information about the carpets in stock */
+/* Create Carpet Countries table to store valid countries of origin for carpets */
+CREATE TABLE Carpet_Countries (
+    PRIMARY KEY (carpet_country),
+    carpet_country  VARCHAR(64) NOT NULL
+);
+
+/* Create Carpet Styles table to store valid styles for carpets */
+CREATE TABLE Carpet_Styles (
+    PRIMARY KEY (carpet_style),
+    carpet_style  VARCHAR(64) NOT NULL
+);
+
+/* Create Carpet Materials table to store valid materials for carpets */
+CREATE TABLE Carpet_Materials (
+    PRIMARY KEY (carpet_material),
+    carpet_material  VARCHAR(64) NOT NULL
+);
+
+/* Create Carpets table to store information about all carpets that have been stocked in the store */
 CREATE TABLE Carpets (
     PRIMARY KEY (carpet_id),
     FOREIGN KEY (carpet_country)  REFERENCES Carpet_Countries(carpet_country),
@@ -25,4 +43,47 @@ CREATE TABLE Carpets (
     carpet_purchase_price       FLOAT (10,2)    NOT NULL,
     carpet_date_acquired        DATE            NOT NULL,
     carpet_markup_percentage    FLOAT (10,2)    NOT NULL
-)
+);
+
+/* Create Customer States table to store valid two-letter state codes for customer addresses */
+CREATE TABLE Customer_States (
+    PRIMARY KEY (customer_state),
+    customer_state  VARCHAR(2) NOT NULL
+);
+
+/* Create Customers table to store information about current and past customers at the store */
+CREATE TABLE Customers (
+    PRIMARY KEY (customer_id),
+    FOREIGN KEY (customer_state) REFERENCES Customer_States(customer_state),
+    customer_id                 INT unsigned    NOT NULL AUTO_INCREMENT,
+    customer_first_name         VARCHAR(64)     NOT NULL,
+    customer_last_name          VARCHAR(64)     NOT NULL,
+    customer_street_address     VARCHAR(128)    NOT NULL, 
+    customer_city               VARCHAR(64)     NOT NULL,
+    customer_state              VARCHAR(2)      NOT NULL,
+    customer_zip                VARCHAR(5)      NOT NULL,
+    customer_phone              VARCHAR(10),
+    customer_activity           VARCHAR(10)
+);
+
+/* Create Carpet Sales table to store information about each carpet sale */
+CREATE TABLE Carpet_Sales (
+    PRIMARY KEY (customer_id, carpet_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    FOREIGN KEY (carpet_id)   REFERENCES Carpets(carpet_id),
+    customer_id     INT unsigned    NOT NULL,
+    carpet_id       INT unsigned    NOT NULL,
+    sale_date       DATE            NOT NULL,
+    sale_price      FLOAT (10,2)    NOT NULL
+);
+
+/* Create Carpet Rentals table to store information about each carpet rental */
+CREATE TABLE Carpet_Rentals (
+    PRIMARY KEY (customer_id, carpet_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    FOREIGN KEY (carpet_id)   REFERENCES Carpets(carpet_id),
+    customer_id             INT unsigned    NOT NULL,
+    carpet_id               INT unsigned    NOT NULL,
+    expected_return_date    DATE            NOT NULL,
+    actual_return_date      DATE
+);
