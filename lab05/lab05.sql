e2c3f93fcea927719658f8247764bc38d9df6024
Rachel <rachelstowe2020@gmail.com>
2023-10-02 21:17:57 +0000

    Edited SQL file

--- lab05/lab05.sql
+++ lab05/lab05.sql
@@ -62,7 +62,7 @@ CREATE TABLE Customers (
     customer_city               VARCHAR(64)     NOT NULL,
     customer_state              VARCHAR(2)      NOT NULL,
     customer_zip                VARCHAR(5)      NOT NULL,
-    customer_phone              VARCHAR(10),
+    customer_phone              VARCHAR(10)     UNIQUE,
     customer_activity           VARCHAR(10)
 );
 
@@ -87,3 +87,127 @@ CREATE TABLE Carpet_Rentals (
     expected_return_date    DATE            NOT NULL,
     actual_return_date      DATE
 );
+
+/* Fill validation tables with data */
+INSERT INTO Carpet_Countries(carpet_country)
+VALUES ('Turkey'),
+       ('Iran'),
+       ('India'),
+       ('Afghanistan');
+
+INSERT INTO Carpet_Styles(carpet_style)
+VALUES ('Ushak'),
+       ('Tabriz'),
+       ('Agra');
+
+INSERT INTO Carpet_Materials(carpet_material)
+VALUES ('Wool'),
+       ('Cotton'),
+       ('Silk');
+
+INSERT INTO Customer_States(customer_state)
+VALUES ('AL'),
+       ('AK'),
+       ('AZ'),
+       ('AR'),
+       ('CA'),
+       ('CO'),
+       ('CT'),
+       ('DE'),
+       ('FL'),
+       ('GA'),
+       ('HI'),
+       ('ID'),
+       ('IL'),
+       ('IN'),
+       ('IA'),
+       ('KS'),
+       ('KY'),
+       ('LA'),
+       ('ME'),
+       ('MD'),
+       ('MA'),
+       ('MI'),
+       ('MN'),
+       ('MS'),
+       ('MO'),
+       ('MT'),
+       ('NE'),
+       ('NV'),
+       ('NH'),
+       ('NJ'),
+       ('NM'),
+       ('NY'),
+       ('NC'),
+       ('ND'),
+       ('OH'),
+       ('OK'),
+       ('OR'),
+       ('PA'),
+       ('RI'),
+       ('SC'),
+       ('SD'),
+       ('TN'),
+       ('TX'),
+       ('UT'),
+       ('VT'),
+       ('VA'),
+       ('WA'),
+       ('WV'),
+       ('WI'),
+       ('WY');
+
+/* Fill Carpets table */
+INSERT INTO Carpets (carpet_country, carpet_style, carpet_material, carpet_width, carpet_length, 
+                    carpet_year, carpet_purchase_price, carpet_date_acquired, carpet_markup_percentage)
+VALUES ('Turkey', 'Ushak',  'Wool', 5,  7,  1925, 625,   20170406, 100),
+       ('Iran',   'Tabriz', 'Silk', 10, 14, 1910, 28000, 20170406, 75),
+       ('India',  'Agra',   'Wool', 8,  10, 2017, 1200,  20170615, 100),
+       ('India',  'Agra',   'Wool', 4,  6,  2017, 450,   20170615, 120);
+
+/* Fill Customers table */
+INSERT INTO Customers (customer_first_name, customer_last_name, customer_street_address, customer_city, 
+                      customer_state, customer_zip, customer_phone, customer_activity)
+VALUES ('Akira',    'Ingram',  '68 Country Drive',       'Roseville',      'MI', '48066', '9262526716', 'Active'),
+       ('Meredith', 'Spencer', '9044 Piper Lane',        'North Royalton', 'OH', '44133', '8175305994', 'Active'),
+       ('Marco',    'Page',    '747 East Harrison Lane', 'Atlanta',        'GA', '30303', '5887996535', 'Active'),
+       ('Sandra',   'Page',    '47 East Harrison Lane',  'Atlanta',        'GA', '30303', '9976972666', 'Active'),
+       ('Gloria',   'Gomez',   '78 Corona Road',         'Fullerton',      'CA', '92831',  NULL,        'Active'),
+       ('Bria',     'Le',      '386 Silver Spear Court', 'Coraopolis',     'PA', '15108',  NULL,        'Active');
+
+/* Fill Carpet Sales table */
+INSERT INTO Carpet_Sales (customer_id, carpet_id, sale_date, sale_price)
+VALUES (5, 1, 20171214, 990),
+       (6, 3, 20171224, 2400);
+
+/* Fill Carpet Rentals table */
+INSERT INTO Carpet_Rentals (customer_id, carpet_id, expected_return_date, actual_return_date)
+VALUES (2, 2, 20171226, 20171226);
+
+/* Display Carpets table */
+SELECT carpet_id AS 'Inventory #',
+       carpet_country AS 'Country',
+       carpet_style AS 'Style',
+       carpet_material AS 'Material',
+       carpet_year AS 'Year of Origin',
+       carpet_width AS 'Width',
+       carpet_length AS 'Length',
+       carpet_purchase_price AS 'Purchase Price',
+       carpet_date_acquired AS 'Date Acquired',
+       carpet_markup_percentage AS 'Markup %',
+       (carpet_purchase_price + carpet_purchase_price * carpet_markup_percentage / 100) AS 'List Price'
+  FROM Carpets;
+    
+/* Display Customers table */
+SELECT customer_first_name AS 'First Name',
+       customer_last_name AS 'Last Name', 
+       customer_street_address AS 'Street Address', 
+       customer_city AS 'City',
+       customer_state AS 'State',
+       customer_zip AS 'Zip Code',
+       customer_phone AS 'Mobile Phone'
+  FROM Customers; 
+
+/* Display Carpet Sales table */
+
+/* Display Carpet Rentals table */
