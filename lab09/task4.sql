ec10902d312bb00cc1d8115942a9030fe599f6a2
Rachel <rachelstowe2020@gmail.com>
2023-11-01 02:37:18 +0000

    Finished lab 9

--- lab09/task4.sql
+++ lab09/task4.sql
@@ -23,21 +23,20 @@ SELECT climb_name,
        USING (climber_id)
        GROUP BY climb_name; 
 
-SELECT * FROM first_climbs;
-
 -- View each climb's name, grade, length, and crag, and if applicable,
 -- its first climber  and/or first equipper
 SELECT climb_name AS 'Name', 
        grade_str AS 'Grade',
-       climb_len_ft AS 'Length (ft)',
+       COALESCE(climb_len_ft, '') AS 'Length (ft)',
        crag_name AS 'Crag',
-       first_ascent AS 'First ascent by',
-       GROUP_CONCAT(DISTINCT CONCAT(climber_first_name, ' ', climber_last_name)) AS 'Equipped by'
+       COALESCE(first_ascent, '') AS 'First ascent by',
+       COALESCE(GROUP_CONCAT(DISTINCT CONCAT(climber_first_name, ' ', climber_last_name)), '') AS 'Equipped by'
   FROM first_climbs
        LEFT OUTER JOIN developed_climbs
        USING (climb_id)
        LEFT OUTER JOIN climbers
-       USING (developed_climbs(climber_id))
-       GROUP BY climb_name;
+       ON developed_climbs.climber_id = climbers.climber_id
+       GROUP BY climb_name
+       ORDER BY crag_name;
 
 DROP TABLE first_climbs;
